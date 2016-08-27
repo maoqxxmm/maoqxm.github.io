a79b03da17fafed14d155cb81f08b12441f40909
毛球 <392818909@qq.com>
2016-08-27 11:53:19 +0800

    换成启源的服务器

---
+++ json.php
@@ -0,0 +1,16 @@
+
+<?php
+$url = $_GET['q'];
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, 'http://live.bilibili.com' + $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$contents = curl_exec($ch);
+curl_close($ch);
+header('Content-Type: text/json;charset=utf-8');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:POST');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+echo $contents;
+?>
